13b25751fe0e027ada0e7c486559eb88bcf5abca
LilPrice-Code <littleprice002@gmail.com>
2019-08-26 11:58:33 -0500

    Completed

---
+++ index.py
@@ -0,0 +1,17 @@
+def main():
+    math(nums(),nums())
+
+def nums():
+    num1 = int(input("Enter a number\n"))
+    return num1
+
+def math(numb1, numb2):
+    print(numb1+numb2)
+    print(numb1-numb2)
+    print(numb1*numb2)
+    print(numb1/numb2)
+
+
+
+
+main()
